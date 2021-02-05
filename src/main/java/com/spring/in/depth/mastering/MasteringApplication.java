39861a8d501970cc557143576f683bf8873c0d2b
e.mubarak <e.mubarak@alwefaq.com>
2021-02-05 23:56:55 +0200

    first push for spring in depth

---
+++ src/main/java/com/spring/in/depth/mastering/MasteringApplication.java
@@ -0,0 +1,23 @@
+package com.spring.in.depth.mastering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableAsync
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
+public class MasteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/in/depth/mastering/MasteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.in.depth.mastering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
