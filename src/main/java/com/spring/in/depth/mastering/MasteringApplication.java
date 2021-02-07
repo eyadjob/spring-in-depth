473851bfe329d0244a4ed75f82acee14625a09d5
Eyad Mubarak <e.mubarak@iyelo.com>
2021-02-07 15:12:00 +0200

    changes on maven

--- src/main/java/com/spring/in/depth/mastering/MasteringApplication.java
+++ src/main/java/com/spring/in/depth/mastering/MasteringApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @SpringBootApplication(exclude = {
